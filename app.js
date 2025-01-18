174aec66d3206ddb15ce69eeae1d53d6e9bcd8fc
Nat <64110681+vidmontng@users.noreply.github.com>
2025-01-18 12:37:19 -0500

    Added designer files

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 const mainRoutes = require('./routes');
--- routes/cards.js
+++ routes/cards.js
@@ -4,23 +4,45 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+
+
+
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+    const flashcardId = Math.floor(Math.random() * numberOfCards);
+    
+    res.redirect(`/cards/${flashcardId}`);
+});
+
+
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     let side = req.query.side;
     const {hint} = cards[id];
     const text = cards[id][side];
     let templateData;
+    const name = req.cookies.username;
 
     if (side==="answer") {
         side = "question";
-        templateData = {text, side, id};
-       } else {
+        templateData = {text, side, id, name};
+       } else if (side==="question"){
         side = "answer";
-        templateData = {text, side, id, hint};
+        templateData = {text, side, id, hint, name};
+       } else if (!side) {
+        res.redirect(`/cards/${id}/?side=question`);
+       } else if (side === "hint") {
+        templateData = {hint};
+
        }
 
     res.render('card', templateData);
 });
+
+
+
+
+
 
 module.exports = router;
 
